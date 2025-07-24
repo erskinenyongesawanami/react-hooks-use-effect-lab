cf60ecd7d3d1b3cc07b1eae4d4cdfe719e167fe1
erskinenyongesawanami <erskineviper@gmail.com>
2025-07-25 00:20:10 +0300

    Add changes made

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      if (timeRemaining > 1) {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      } else {
+        onAnswered(false);
+        setTimeRemaining(10);
+      }
+    }, 1000);
+
+    return () => clearTimeout(timeoutId);
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
